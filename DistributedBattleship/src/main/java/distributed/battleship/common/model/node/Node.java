e432b72697974a45704ece867d30db83f03327ba
Viola Leonardo <leonardo.viola2@studio.unibo.it>
2026-05-16 10:32:10 +0200

    added class node

---
+++ DistributedBattleship/src/main/java/distributed/battleship/common/model/node/Node.java
@@ -0,0 +1,57 @@
+package distributed.battleship.common.model.node;
+
+/**
+ * Represents a generic node in the network.
+ * Contains the basic node information: immutable node ID, IP address and port.
+ */
+public class Node {
+    private UUID nodeId;
+    protected String ip;
+
+    /**
+     * Node constructor.
+     *
+     * @param ip Node IP address
+     */
+    public Node(String ip) {
+        this.nodeId = UUID.randomUUID();
+        this.ip = ip;
+    }
+
+    protected Node(UUID nodeId, String ip) {
+        this.nodeId = nodeId;
+        this.ip = ip;
+    }
+
+
+
+    public UUID getNodeId() {
+        return nodeId;
+    }
+
+    /**
+     * Gets the node IP address.
+     *
+     * @return Node IP
+     */
+    public String getIp() {
+        return ip;
+    }
+
+    /**
+     * Sets the node IP address.
+     *
+     * @param ip IP address
+     */
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "nodeId=" + nodeId +
+                ", ip='" + ip + '\'' +
+                '}';
+    }
+}
