dde22e0bde4c6faefccbe52bdf2ed949f46537a7
Viola Leonardo <leonardo.viola2@studio.unibo.it>
2026-06-10 10:07:11 +0200

    added task to generate fat jar

--- DistributedBattleship/build.gradle.kts
+++ DistributedBattleship/build.gradle.kts
@@ -19,3 +19,18 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.register<Jar>("fatJar") {
+    manifest {
+        // Modifica inserendo il package e la classe principale esatta del tuo progetto
+        attributes["Main-Class"] = "distributed.battleship.Main"
+    }
+
+    archiveBaseName.set("DistributedBattleship-all")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    // Questa è la sintassi specifica Kotlin DSL per scompattare e unire le dipendenze
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+
+    with(tasks.jar.get())
+}
